6b7b59de4627cc90aa4605d4dadeaa9fe902e642
IanJWilson <ianjwilson8@outlook.com>
2018-11-13 13:06:34 -0500

    the game

---
+++ RacingGame/Assets/Scripts/Car.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public float maxTurnAngle = 10;
+    public float maxTorque = 10;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    public WheelCollider wheelBL;
+    public WheelCollider wheelBR;
+
+    // FixedUpdate is called once per physics frame
+    void FixedUpdate()
+    {
+        //front wheel steering
+        wheelFL.steerAngle = Input.GetAxis("Horizontal") * maxTurnAngle;
+        wheelFR.steerAngle = Input.GetAxis("Horizontal") * maxTurnAngle;
+        //rear wheel drive
+        wheelBL.motorTorque = Input.GetAxis("Vertical") * maxTorque;
+        wheelBR.motorTorque = Input.GetAxis("Vertical") * maxTorque;
+    }
+
+}
---
+++ RacingGame/Assets/Scripts/FinishLineScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLineScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       /* if OnTriggerOverlap(OnTriggerOverlap, other)
+        {
+
+        }
+        ian is a poopy head with a smol peen. teehee.*/
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
